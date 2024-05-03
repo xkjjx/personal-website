412b466a8c37eee35f3c4d26ad6ddc9fe3e9dc60
Kevin Johnson <john2003.kjj@gmail.com>
2024-05-02 23:44:08 -0500

    updated database for blogs and guides

--- database/init.sql
+++ database/init.sql
@@ -42,3 +42,60 @@ CREATE TABLE project-skills-relation {
     FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (skill_id) REFERENCES skills(id)
 };
+
+CREATE TABLE blogs{
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    category VARCHAR(255),
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
+
+CREATE TABLE blog-skills-relation {
+    id SERIAL PRIMARY KEY,
+    blog_id INT,
+    skill_id INT,
+    FOREIGN KEY (blog_id) REFERENCES blogs(id),
+    FOREIGN KEY (skill_id) REFERENCES skills(id)
+};
+
+CREATE TABLE blog-experiences-relation {
+    id SERIAL PRIMARY KEY,
+    blog_id INT,
+    experience_id INT,
+    FOREIGN KEY (blog_id) REFERENCES blogs(id),
+    FOREIGN KEY (experience_id) REFERENCES experiences(id)
+};
+
+CREATE TABLE blog-projects-relation {
+    id SERIAL PRIMARY KEY,
+    blog_id INT,
+    project_id INT,
+    FOREIGN KEY (blog_id) REFERENCES blogs(id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+};
+
+CREATE TABLE guides {
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    category VARCHAR(255),
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
+
+CREATE TABLE guide-part {
+    id SERIAL PRIMARY KEY,
+    guide_id INT,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    part_number INT,
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (guide_id) REFERENCES guides(id)
+};
+
+CREATE TABLE guide-skills-relation {
+    id SERIAL PRIMARY KEY,
+    guide_id INT,
+    skill_id INT,
+    FOREIGN KEY (guide_id) REFERENCES guides(id),
+    FOREIGN KEY (skill_id) REFERENCES skills(id)
+};
