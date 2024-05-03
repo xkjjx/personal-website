74e1ca87df586d8087f9dc65c0a983e9e2e8fac1
Kevin Johnson <john2003.kjj@gmail.com>
2024-05-03 10:00:58 -0500

    added basic frontend next.js base

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+
+    <div className="justify-center">
+      <h1 className="text-center">Home</h1>
+    </div>
+    </main>
+  );
+}
